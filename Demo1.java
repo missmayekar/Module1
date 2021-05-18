696512d8d778b06d8e942fc0f027e50bd89c8bea
missmayekar <nehamayekar1999@gmail.com>
2021-05-18 12:58:25 +0530

    Module 1 third commit

--- Demo1.java
+++ Demo1.java
@@ -2,5 +2,6 @@ class Demo1{
 public static void main(String[] args){
 System.out.println("This is Module 1");
 System.out.println("This is to check the trigger!");
+System.out.println("This is to check 2!");
 }
 }
